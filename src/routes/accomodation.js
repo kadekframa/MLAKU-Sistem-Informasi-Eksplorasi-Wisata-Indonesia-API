a2af15d928a95dcfc23e3b5ddaff1512c0a8cf5d
aprlfch <aprlianfachrul@gmail.com>
2022-07-03 13:22:28 +0800

    Fix error in update Accomodation

--- src/routes/accomodation.js
+++ src/routes/accomodation.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {body} = require('express-validator');
+const { body } = require('express-validator');
 
 const accomodationController = require('../controllers/accomodation')
 
@@ -7,21 +7,21 @@ const router = express.Router();
 
 // [POST] : v1/accomodation
 router.post('/', [
-    body('accomodation_name').isLength({min: 5})
-    .withMessage('Input accomodation_name minimum 5 karakter'),
-    body('address').isLength({min: 5})
-    .withMessage('Input address minimum 5 karakter'),
-    body('desc').isLength({min: 5})
-    .withMessage('Input desc minimum 5 karakter'),
-    body('open_time').isLength({min: 5})
-    .withMessage('Input open_time minimum 5 karakter'),
-    body('open_day').isLength({min: 5})
-    .withMessage('Input open_day minimum 5 karakter'),
-    body('contact_number').isLength({min: 5})
-    .withMessage('Input contact_number minimum 5 karakter')
-    ], accomodationController.createAccomodation);
+    body('accomodation_name').isLength({ min: 5 })
+        .withMessage('Input accomodation_name minimum 5 karakter'),
+    body('address').isLength({ min: 5 })
+        .withMessage('Input address minimum 5 karakter'),
+    body('desc').isLength({ min: 5 })
+        .withMessage('Input desc minimum 5 karakter'),
+    body('open_time').isLength({ min: 5 })
+        .withMessage('Input open_time minimum 5 karakter'),
+    body('open_day').isLength({ min: 5 })
+        .withMessage('Input open_day minimum 5 karakter'),
+    body('contact_number').isLength({ min: 5 })
+        .withMessage('Input contact_number minimum 5 karakter')
+], accomodationController.createAccomodation);
 
-    
+
 // [GET] : v1/accomodation
 router.get('/', accomodationController.getAllAccomodation);
 
@@ -30,19 +30,19 @@ router.get('/:postId', accomodationController.getAccomodationById);
 
 // [PUT] : v1/accomodation/:postId
 router.put('/:postId', [
-    body('_name').isLength({min: 5})
-    .withMessage('Input dest_name minimum 5 karakter'),
-    body('address').isLength({min: 5})
-    .withMessage('Input address minimum 5 karakter'),
-    body('desc').isLength({min: 5})
-    .withMessage('Input desc minimum 5 karakter'),
-    body('open_time').isLength({min: 5})
-    .withMessage('Input open_time minimum 5 karakter'),
-    body('open_day').isLength({min: 5})
-    .withMessage('Input open_day minimum 5 karakter'),
-    body('contact_number').isLength({min: 5})
-    .withMessage('Input contact_number minimum 5 karakter')
-    ], accomodationController.updateAccomodation);
+    body('accomodation_name').isLength({ min: 5 })
+        .withMessage('Input accomodation_name minimum 5 karakter'),
+    body('address').isLength({ min: 5 })
+        .withMessage('Input address minimum 5 karakter'),
+    body('desc').isLength({ min: 5 })
+        .withMessage('Input desc minimum 5 karakter'),
+    body('open_time').isLength({ min: 5 })
+        .withMessage('Input open_time minimum 5 karakter'),
+    body('open_day').isLength({ min: 5 })
+        .withMessage('Input open_day minimum 5 karakter'),
+    body('contact_number').isLength({ min: 5 })
+        .withMessage('Input contact_number minimum 5 karakter')
+], accomodationController.updateAccomodation);
 
 // [DELETE] : v1/accomodation/:postId
 router.delete('/:postId', accomodationController.deleteAccomodation)
